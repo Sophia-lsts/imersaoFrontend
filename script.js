aa92f5df2aa4cbd69b6a7784ff3a96f3f4ab9ffe
Sophia <sophialimasantos4@gmail.com>
2025-02-03 11:52:19 -0300

    erro de pesquisa resolvido, falta estilizar

--- script.js
+++ script.js
@@ -3,38 +3,57 @@ const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-
-    const url = `http://localhost:1000/artists?name_like=${searchTerm}`
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
     fetch(url)
         .then((response) => response.json())
-        .then((result) => displayResults(result))
-
+        .then((result) => displayResults(result));
 }
 
 function displayResults(result) {
-
     resultPlaylist.classList.add("hidden");
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+    
+    const artistContainer = document.getElementById('result-artist');
+    artistContainer.innerHTML = '';
 
-    result.forEach(element => {
-        artistName.innerText = element.name;
-        artistImage.src = element.urlImg;
-    });
+    if (result.length > 0) {
+        const searchTerm = searchInput.value.toLowerCase(); 
 
-    resultArtist.classList.remove('hidden');
+        const filteredResults = result.filter(artist => 
+            artist.name.toLowerCase().includes(searchTerm)
+        );
 
+        filteredResults.forEach(artist => {
+            const artistDiv = document.createElement('div');
+            artistDiv.classList.add('artist-card__searched');
+
+            const artistName = document.createElement('h2');
+            artistName.classList.add('artist-name__searched');
+
+            const artistImg = document.createElement('img');
+            artistImg.classList.add('artist-img__searched');
+
+            artistName.innerText = artist.name;
+            artistImg.src = artist.urlImg;
+
+            artistDiv.appendChild(artistImg);
+            artistDiv.appendChild(artistName);
+            artistContainer.appendChild(artistDiv);
+        });
+
+        artistContainer.classList.remove('hidden');
+    } else {
+        artistContainer.classList.add('hidden');
+    }
 }
 
 document.addEventListener('input', function () {
-
     const searchTerm = searchInput.value.toLowerCase();
+
     if (searchTerm === '') {
         resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
+        resultArtist.classList.add('hidden');
         return;
     }
     
     requestApi(searchTerm);
-
-})
+});
